1d7a700a948041d614a902964a0caa1493956b33
Electressic <87477650+Electressic@users.noreply.github.com>
2022-03-21 16:07:50 +0100

    Add files via upload

---
+++ Spielfigur.java
@@ -0,0 +1,27 @@
+package spiel;
+
+public class Spielfigur {
+    private int id;
+    public String rang;
+
+    public Spielfigur(int id, String rang) {
+        this.id = id;
+        this.rang = rang;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRang() {
+        return rang;
+    }
+
+    public void setRang(String rang) {
+        this.rang = rang;
+    }
+}
