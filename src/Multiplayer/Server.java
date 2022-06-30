1b6dcf314b245c50ab22ee9e496115a3c5cbec1c
NotAND-Or <102046660+NotAND-Or@users.noreply.github.com>
2022-06-30 15:58:05 +0200

    Commit

---
+++ src/Multiplayer/Server.java
@@ -0,0 +1,33 @@
+package progprak.src.Multiplayer;
+import progprak.src.ComProto;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException
+    {
+        try (
+            ServerSocket Server = new ServerSocket(111);
+            Socket clientsocket = Server.accept();
+            BufferedReader incoming = new BufferedReader(new InputStreamReader(clientsocket.getInputStream()));
+            BufferedWriter outgoing = new BufferedWriter(new OutputStreamWriter(clientsocket.getOutputStream()));
+        )
+        {
+            String input, output;
+            ComProto CP = new ComProto();
+            while ((input = incoming.readLine()) != null)
+            {
+                output = CP.Communication(input);
+                outgoing.write(output);
+                outgoing.newLine();
+                outgoing.flush();
+            }
+        }
+
+        catch (IOException e) {
+            System.out.println("Exception in Server main");
+        }
+    }
+}
